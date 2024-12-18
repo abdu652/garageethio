fab8f80d395dec71f5bc0b73caa877bdf651bf04
Abdu201213 <sabdu9049@gamil.com>
2024-12-18 21:56:20 +0300

    set the token into localStorage

--- frontend/garage-project/src/services/Login.service.jsx
+++ frontend/garage-project/src/services/Login.service.jsx
@@ -3,7 +3,6 @@
 async function Login(loginForm){
 
    try{
-   
       const response = await fetch("http://localhost:3000/login", {
          method: "POST",
          headers: {
@@ -16,7 +15,11 @@ async function Login(loginForm){
         return {message:response.statusText}
       } 
       const result = await response.json();
-      return result;            
+      const {isAuthenticated, token} = result;
+      if(isAuthenticated){
+         token && localStorage.setItem("employee",JSON.stringify(token));
+         return result;            
+      }
    }      
    catch (err) {
       return {message:err.message}
